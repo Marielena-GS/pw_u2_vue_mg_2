e1082de2686a09c0956692f91f4a0781189bea71
pokedex <mgonzalez@uce.edu.ec>
2025-11-28 09:25:42 -0500

    Commit

--- src/views/ContadorView.vue
+++ src/views/ContadorView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Contador tit="Contador 1" v-bind:inicio="4" :mostrar="true"/>
-    <Contador tit="Contador 2" :inicio="15" :mostrar="false" />
+    <Contador tit="Contador 2" :inicio="15" :mostrar="true" />
   </div>
 </template>
 <script>
